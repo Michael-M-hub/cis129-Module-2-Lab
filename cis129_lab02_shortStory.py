2514ea5e4ae445986a4823e8d311ed67059278da
Michael-M-hub <mmiao1@mail.pima.edu>
2024-09-19 15:26:19 -0700

    Update cis129_lab02_shortStory.py
    I delete the print("Hello, World!"), and add a different print() statements.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print("hello world!")
+# Author: Michael Miao
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("Hi my name is Michael, this is my first year in pima, my major is computer science.")
